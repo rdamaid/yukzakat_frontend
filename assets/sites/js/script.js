83758d76d96cb518604b57093e06624a3531cc16
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2020-09-28 11:35:22 +0700

    beranda

---
+++ assets/sites/js/script.js
@@ -0,0 +1,12 @@
+$(window).scroll(function () {
+    console.log("TOLOL")
+//   var wScroll = $(this).scrollTop();
+
+//   $(".jumbotron h1").css({
+//     "transform" : "translate(0px, " + wScroll / 2 + "%)"
+//   });
+
+//   $(".jumbotron p").css({
+//     "transform" : "translate(0px, " + wScroll / 2 + "%)"
+//   });
+});
