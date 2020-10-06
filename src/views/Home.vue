c2c44a17c6be1b2dbd6651822795ffe663ceac94
mkamilmistar <kamil_mistar@apps.ipb.ac.id>
2020-10-06 11:07:00 +0700

    inisiasi vue

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Selamat Datang di Development YukZakat" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
